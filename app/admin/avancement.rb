800f99a31fcc8654e1e466954aab4fd0134c8128
Ghislain Chavanne <gh.chavanne@gmail.com>
2017-06-19 09:09:34 +0200

    last changes

--- app/admin/avancement.rb
+++ app/admin/avancement.rb
@@ -1,15 +1,23 @@
 ActiveAdmin.register Avancement do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+  menu label: "Avancements"
+
+  index do
+    selectable_column
+    column :title
+    column :description
+    column :date
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    inputs 'Details' do
+      input :title, placeholder: "Titre"
+      input :description
+      input :date, placeholder: "DAY/MONTH/YEAR"
+      f.actions
+    end
+  end
 
 end
